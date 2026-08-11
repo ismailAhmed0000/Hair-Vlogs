c12222ae18e78c17a88a6323e36a0ad21754e981
Ismail Ahmed <143304919+ismailAhmed0000@users.noreply.github.com>
2026-08-11 18:17:57 +0500

    mobile app setup

---
+++ mobile/App.tsx
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { queryClient } from './src/lib/queryClient';
+
+import './global.css';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppContent />
+      </SafeAreaProvider>
+    </QueryClientProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View className="flex-1" style={styles.container}>
+      <NewAppScreen
+        templateFileName="App.tsx"
+        safeAreaInsets={safeAreaInsets}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
